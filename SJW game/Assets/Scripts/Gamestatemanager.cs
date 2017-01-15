8fa3e53fae8dcc2af57ff4cf610a94b5c17f8df3
Joshuabriggs <greedyoljosh@gmail.com>
2017-01-15 15:12:51 +0000

    Got the animation working, created the scene manager script

---
+++ SJW game/Assets/Scripts/Gamestatemanager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Gamestatemanager : MonoBehaviour {
+
+    public StageManager sManager;
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public static Gamestatemanager instance = null;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("A Gamstatemanager instance already exists!");
+        }
+        instance = this;
+    }
+}
---
+++ SJW game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField]
+    float m_movementSpeed = 2.0f;
+    private bool keyLeft, keyRight, keyUp, keyDown, keySprint;
+    private Transform m_transform;
+
+	// Use this for initialization
+	void Start () {
+        m_transform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        GetInput();
+	
+	}
+
+    private void GetInput()
+    {
+        //Move Left
+        if (Input.GetKey(KeyCode.A))
+        {
+            keyLeft = true;
+            
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            keyLeft = false;
+        }
+        //Move Right
+        if (Input.GetKey(KeyCode.D))
+        {
+            keyRight = true;
+            
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            keyRight = false;
+        }
+
+        //Move Up
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            keyUp = true;
+           
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            keyUp = false;
+        }
+        //Move Down
+        if (Input.GetKey(KeyCode.S))
+        {
+            keyDown = true;
+  
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            keyDown = false;
+        }
+        //Sprint
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            keySprint = true;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            keySprint = false;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+
+        //Move Left
+        if (keyLeft)
+        {
+            m_transform.Translate(m_transform.right * -m_movementSpeed * Time.deltaTime);
+        }
+        //Move Right
+        if (keyRight)
+        {
+            m_transform.Translate(m_transform.right * m_movementSpeed * Time.deltaTime);
+        }
+        //Move Up
+        if (keyUp)
+        {
+            m_transform.Translate(m_transform.up * m_movementSpeed * Time.deltaTime);
+        }
+        //Move Down
+        if (keyDown)
+        {
+            m_transform.Translate(m_transform.up * -m_movementSpeed * Time.deltaTime);
+        }
+        //Sprint
+        if (keySprint)
+        {
+            m_movementSpeed  = 2f;
+        }
+        if (!keySprint)
+        {
+            m_movementSpeed = 1f;
+        }
+    }
+}
+
